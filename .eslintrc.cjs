97df4401381f58621f58f464c3d060fa8900048d
Gabriel <adrastopoulos@gmail.com>
2024-04-18 14:42:45 -0400

    ci: add prettier to ci

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,71 +8,72 @@ const tsDirs = ['public/src', 'src', 'test'];
 
 // Helper walk function to check all directories
 function walk(dir) {
-    let results = [];
-    const list = fs.readdirSync(dir);
-    list.forEach((file) => {
-        file = `${dir}/${file}`;
-        const stat = fs.statSync(file);
-        if (stat && stat.isDirectory()) {
-            results = results.concat(walk(file));
-        } else {
-            results.push(file);
-        }
-    });
-    return results;
+  let results = [];
+  const list = fs.readdirSync(dir);
+  list.forEach((file) => {
+    file = `${dir}/${file}`;
+    const stat = fs.statSync(file);
+    if (stat && stat.isDirectory()) {
+      results = results.concat(walk(file));
+    } else {
+      results.push(file);
+    }
+  });
+  return results;
 }
 
 // Find all JS files that were compiled from TS
 function find_compiled_js() {
-    let jsFilesList = [];
+  let jsFilesList = [];
 
-    tsDirs.forEach((tsDir) => {
-        const filesList = walk(tsDir);
-        const tsFilesList = filesList.filter(file => path.extname(file).toLowerCase() === '.ts');
-        jsFilesList = jsFilesList.concat(
-            filesList.filter(
-                file => path.extname(file).toLowerCase() === '.js' &&
-          tsFilesList.find(tsFile => tsFile === `${file.replace(/\.[^/.]+$/, '')}.ts`) !== undefined
-            )
-        );
-    });
+  tsDirs.forEach((tsDir) => {
+    const filesList = walk(tsDir);
+    const tsFilesList = filesList.filter((file) => path.extname(file).toLowerCase() === '.ts');
+    jsFilesList = jsFilesList.concat(
+      filesList.filter(
+        (file) =>
+          path.extname(file).toLowerCase() === '.js' &&
+          tsFilesList.find((tsFile) => tsFile === `${file.replace(/\.[^/.]+$/, '')}.ts`) !== undefined
+      )
+    );
+  });
 
-    if (jsFilesList.length === 0) return '';
-    return jsFilesList;
+  if (jsFilesList.length === 0) return '';
+  return jsFilesList;
 }
 
 module.exports = {
-    extends: ['nodebb'],
-    root: true,
-    ignorePatterns: find_compiled_js(),
-    rules: {
-        indent: ['error', 4],
-        'max-len': 'off',
-        'no-mixed-operators': 'off',
-        'no-useless-concat': 'off',
+  extends: ['nodebb'],
+  root: true,
+  ignorePatterns: find_compiled_js(),
+  rules: {
+    indent: ['error', 4],
+    'max-len': 'off',
+    'no-mixed-operators': 'off',
+    'no-useless-concat': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint', 'prettier/recommended'],
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+      },
+      rules: {
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': 'error',
+      },
     },
-    overrides: [
-        {
-            files: ['**/*.ts', '**/*.tsx'],
-            extends: [
-                'plugin:@typescript-eslint/recommended',
-                'plugin:@typescript-eslint/recommended-requiring-type-checking',
-            ],
-            parser: '@typescript-eslint/parser',
-            plugins: ['@typescript-eslint', 'prettier/recommended'],
-            parserOptions: {
-                ecmaFeatures: { jsx: true },
-                project: './tsconfig.json',
-            },
-            rules: {
-                'no-use-before-define': 'off',
-                '@typescript-eslint/no-use-before-define': 'error',
-            },
-        },
-    ],
-    env: {
-        node: true,
-        es6: true,
-        jquery: true,
-    },
+  ],
+  env: {
+    node: true,
+    es6: true,
+    jquery: true,
+  },
 };
